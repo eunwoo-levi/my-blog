59353d4f6bb5c4c7c117391c8c1a49e905220567
eunwoo-levi <eunwoo1341@gmail.com>
2024-09-09 22:27:59 +0900

    fixed: deleted console.log

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,16 +2,13 @@ import { getAllPostsMeta } from "@/lib/mdx";
 import Categories from "./_components/Categories";
 import PaginationComponent from "@/components/PaginationComponent";
 
-export const revalidate = 10; // ISR
+export const revalidate = 7200; // ISR
 
 export default async function PostsPage() {
   const posts = await getAllPostsMeta();
 
   // 서버에서 페이지가 렌더링된 시간을 저장
   const now = new Date().toLocaleString();
-
-  // 콘솔에 렌더링 시간 출력
-  console.log("Page rendered at:", now);
 
   return (
     <main className="w-full min-h-screen flex flex-col justify-center items-center px-[5px] lg:px-0">
