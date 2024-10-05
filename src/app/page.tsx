a4df7074da0c8592e38d305b6babf0bfb6a413a1
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-06 00:10:58 +0900

    refactor: use lazy for importing Earch component in Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,96 @@
-import Earth from "@/components/Earth";
 import { ShootingStars } from "@/components/ui/shooting-stars";
 import { StarsBackground } from "@/components/ui/stars-background";
 import { MdOutlineEmail } from "react-icons/md";
 import Link from "next/link";
+import { lazy, Suspense } from "react";
+import Head from "next/head";
+
+const Earth = lazy(() => import("@/components/Earth"));
+
+interface CustomLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const CustomLink: React.FC<CustomLinkProps> = ({ href, children }) => (
+  <Link
+    href={href}
+    className="flex justify-center items-center w-[300px] lg:w-[500px] h-[50px] border rounded-3xl transform duration-200 hover:bg-slate-700 hover:bg-opacity-30 hover:scale-110 cursor-pointer"
+    aria-label={`Navigate to ${String(children)} page`}
+  >
+    {children}
+  </Link>
+);
 
 export default function Home() {
   return (
-    <main className="relative w-full min-h-screen flex flex-col">
-      <StarsBackground />
-      <ShootingStars />
-      <div className="relative flex flex-col items-center justify-between px-[50px] lg:flex-row">
-        <div className="flex flex-col">
-          <h1 className="font-bold text-[50px] pl-[60px] mb-[40px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-700 rounded-lg text-transparent bg-clip-text">
-            PORTFOLIO <br />
-            Frontend Web Developer
-          </h1>
-          <div className="">
-            <div className="border border-slate-800 mb-[50px]" />
-            <h2 className="text-[25px] mb-[20px] font-semibold">
-              Welcome to My Portfolio.
-            </h2>
-            <h3 className="text-[20px]">
-              I am committed to growing as a skilled and professional frontend
-              developer.
-              <br />
-              I dedicate myself to learning new technologies and mastering them,
-              prioritizing User eXperience.
-              <br /> <br />
-              While my primary focus is on Frontend development, <br />
-              I am also expanding my expertise in Backend technologies.
-              <br /> <br />
-              Additionally, I am fluent in English and capable of collaborating
-              effectively on international projects.
-            </h3>
+    <>
+      <Head>
+        <title>My Portfolio - Frontend Web Developer</title>
+        <meta
+          name="description"
+          content="Portfolio of a skilled Frontend Web Developer, showcasing projects and expertise in web development."
+        />
+        <meta
+          name="keywords"
+          content="Frontend, Web Developer, Portfolio, React, Next.js, TypeScript, tech blog"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="relative w-full min-h-screen flex flex-col">
+        <StarsBackground />
+        <ShootingStars />
+        <div className="relative flex flex-col items-center justify-between px-[50px] lg:flex-row">
+          <div className="flex flex-col">
+            <h1 className="font-bold text-[50px] pl-[60px] mb-[40px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-700 rounded-lg text-transparent bg-clip-text">
+              PORTFOLIO <br />
+              Frontend Web Developer
+            </h1>
+            <div className="">
+              <div className="border border-slate-800 mb-[50px]" />
+              <h2 className="text-[25px] mb-[20px] font-semibold">
+                Welcome to My Portfolio.
+              </h2>
+              <h3 className="text-[20px]">
+                I am committed to growing as a skilled and professional frontend
+                developer.
+                <br />
+                I dedicate myself to learning new technologies and mastering
+                them, prioritizing User eXperience.
+                <br /> <br />
+                While my primary focus is on Frontend development, <br />
+                I am also expanding my expertise in Backend technologies.
+                <br /> <br />
+                Additionally, I am fluent in English and capable of
+                collaborating effectively on international projects.
+              </h3>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center justify-between lg:flex-row w-full overflow-hidden lg:pl-[40px]">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Earth className="w-full h-auto" />
+            </Suspense>
           </div>
         </div>
-        <div className="relative flex flex-col items-center justify-between lg:flex-row w-full overflow-hidden lg:pl-[40px]">
-          <Earth className="w-full h-auto" />
+        <div className="flex flex-col justify-center items-center gap-[30px] mt-[150px]">
+          <CustomLink href="/about">About Me</CustomLink>
+          <CustomLink href="/posts">Blog</CustomLink>
+          <CustomLink href="/board">Board</CustomLink>
         </div>
-      </div>
-      <div className="flex flex-col justify-center items-center gap-[30px] mt-[150px]">
-        <Link
-          href={"/about"}
-          className="flex justify-center items-center w-[300px] lg:w-[500px] h-[50px] border  rounded-3xl transform duration-200 hover:bg-slate-700 hover:bg-opacity-30 hover:scale-110 cursor-pointer"
-        >
-          About Me
-        </Link>
-        <Link
-          href={"/posts"}
-          className="flex justify-center items-center w-[300px] lg:w-[500px] h-[50px] border  rounded-3xl transform duration-200 hover:bg-slate-700 hover:bg-opacity-30 hover:scale-110 cursor-pointer"
-        >
-          Blog
-        </Link>
-        <Link
-          href={"/board"}
-          className="flex justify-center items-center w-[300px] lg:w-[500px] h-[50px] border  rounded-3xl transform duration-200 hover:bg-slate-700 hover:bg-opacity-30 hover:scale-110 cursor-pointer"
-        >
-          Board
-        </Link>
-      </div>
-      <div className="z-10 text-white  w-full h-[200px] bg-slate-900 mt-[200px] flex flex-col justify-center items-start pl-[20px] gap-[10px]">
-        <div className="flex gap-[20px]">
-          <MdOutlineEmail size={25} />
-          <a
-            href="mailto:eunwoo1341@gmail.com"
-            className="text-[15px] hover:underline"
-          >
-            eunwoo1341@gmail.com
-          </a>
+        <div className="z-10 text-white  w-full h-[200px] bg-slate-900 mt-[200px] flex flex-col justify-center items-start pl-[20px] gap-[10px]">
+          <div className="flex gap-[20px]">
+            <MdOutlineEmail size={25} />
+            <a
+              href="mailto:eunwoo1341@gmail.com"
+              className="text-[15px] hover:underline"
+            >
+              eunwoo1341@gmail.com
+            </a>
+          </div>
+          <h1 className="text-[15px]">Copyright ⓒ S. Eunwoo</h1>
         </div>
-        <h1 className="text-[15px]">Copyright ⓒ S. Eunwoo</h1>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
