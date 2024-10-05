4ed688c336e213ae89ae8856bdc8a944cee12a53
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-06 04:43:17 +0900

    refactor: add className: prose lg:prose-2xl overflow-hidden break-words for content in slugPage

--- src/app/posts/[category]/[slug]/page.tsx
+++ src/app/posts/[category]/[slug]/page.tsx
@@ -39,11 +39,11 @@ const SlugPage = async ({ params }: { params: Params }) => {
   return (
     <main className="w-full min-h-screen flex flex-col justify-center items-center">
       <div className="mb-[40px]">
-        <h1 className="bg-gradient-to-r from-purple-500 via-pink-400 to-blue-300 px-10 py-4 rounded-xl text-[35px] mt-[50px] font-bold">
+        <h1 className="bg-gradient-to-r from-purple-500 via-pink-400 to-blue-300 px-10 py-4 rounded-xl text-[20px] lg:text-[35px] mt-[50px] font-bold">
           {meta.title}
-        </h1>{" "}
+        </h1>
       </div>
-      <div className="w-full lg:w-[75%] py-[120px] flex flex-col justify-center px-[10px] lg:px-[80px] mb-[80px] border border-slate-700 rounded-xl prose-lg overflow-x-auto">
+      <div className="w-full lg:w-3/4 py-[120px] flex flex-col justify-center px-[10px] lg:px-[80px] mb-[80px] border border-slate-700 rounded-xl prose lg:prose-2xl overflow-hidden break-words">
         {content}
       </div>
       <Giscus />
