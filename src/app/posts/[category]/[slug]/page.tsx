4ca04ea45b880eb89ec6649e7c47e859d828df7c
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-08 02:44:44 +0900

    refactor: prone max width size to 1800px in SlugPage

--- src/app/posts/[category]/[slug]/page.tsx
+++ src/app/posts/[category]/[slug]/page.tsx
@@ -43,7 +43,7 @@ const SlugPage = async ({ params }: { params: Params }) => {
           {meta.title}
         </h1>
       </div>
-      <div className="w-full lg:w-3/4 py-[120px] flex flex-col justify-center px-[10px] lg:px-[80px] mb-[80px] border border-slate-700 rounded-xl prose dark:prose-invert lg:prose-2xl overflow-hidden break-words">
+      <div className="w-full lg:w-3/4 max-w-[1800px] py-[120px] flex flex-col justify-center px-[10px] lg:px-[80px] mb-[80px] border border-slate-700 rounded-xl prose dark:prose-invert lg:prose-2xl overflow-hidden break-words">
         {content}
       </div>
       <Giscus />
