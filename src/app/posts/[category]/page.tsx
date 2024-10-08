540a087f71a573245b97af7be7dcdb0683b91f96
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-09 04:05:43 +0900

    refactor: responsive UI for cagegory title in CagegryPage

--- src/app/posts/[category]/page.tsx
+++ src/app/posts/[category]/page.tsx
@@ -13,7 +13,7 @@ export default async function CategoryPage({ params }: Props) {
 
   return (
     <div className="w-full min-h-screen flex flex-col items-center">
-      <div className="inline-flex items-center border border-blue-500 text-[35px] font-bold rounded-3xl mt-[80px] mb-[200px] p-2">
+      <div className="w-[500px] md:w-[700px] lg:w-[1000px] text-center bg-gradient-to-r from-purple-500 via-pink-400 to-blue-300 px-10 py-6 rounded-xl text-[20px] lg:text-[35px] font-bold my-[100px]  mb-[150px]">
         {params.category}
       </div>
       {posts.length > 0 ? (
