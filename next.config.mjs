36a8ee34d92fe875f7721dc5d97f54fb47c51b46
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-17 00:07:06 +0900

    Add: algorithm a-1.mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 import createMDX from "@next/mdx";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  images: {
-    formats: ["image/avif", "image/webp"],
-  },
+
   /*
   webpack(config, { isServer }) {
     if (!isServer) {
