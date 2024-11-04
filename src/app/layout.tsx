e8995248616fe3dad31f9ada24d92d94e898649a
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-04 22:15:08 +0900

    refactor(navbar): responsive ui for navbar in sm size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/navbar/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
