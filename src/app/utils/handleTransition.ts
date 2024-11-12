c4d53434c3b5b7a7e90bea52c8c6d001ce32a1f8
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-12 16:42:14 +0900

    refactor(global.css): change background opacity

--- src/app/utils/handleTransition.ts
+++ src/app/utils/handleTransition.ts
@@ -5,11 +5,11 @@ export const handleTransition = async (href:string, router:ReturnType<typeof use
     body?.classList.add('page-transition');
 
 
-    const transition = router.push(href);
+    const pageTransition = router.push(href);
 
         // 페이지 전환과 최소 애니메이션 시간을 모두 기다림
         await Promise.all([
-            transition,
+            pageTransition,
             sleep(400)
         ]);
 
