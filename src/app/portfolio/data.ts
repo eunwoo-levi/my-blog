5fc287ae15e3af97c9c7ce3baa72d53cb6b44770
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-27 01:00:10 +0900

    update: portfolio update

--- src/app/portfolio/data.ts
+++ src/app/portfolio/data.ts
@@ -1,86 +1,95 @@
 export interface ProfileProps {
-    name: string;
-    birthDate: string;
-    location: string;
-    email: string;
-    university: string;
-    major: string;
-    achievements: string[];
-  }
-  
-  export interface ActivityProps {
-    title: string;
-    period: string;
-    role?: string; 
-  }
-  
-  export const profileData: ProfileProps = {
-    name: "Seong, Eunwoo",
-    birthDate: "2001. 01. 01",
-    location: "Republic of Korea, Seoul (Daegu)",
-    email: "Eunwoo1341@gmail.com",
-    university: "Kyungbook National University",
-    major: " - Computer Science",
-    achievements: [
-      "TOEIC - 935",
-      "Computer Specialist in Spreadsheet & Database (컴퓨터활용능력 1급)"
-    ]
-  };
-  
-  export const extracurricularData: ActivityProps[] = [
-    {
-        title: "Korean Augmentation To the United States Army, KATUSA",
-        period: "2020.09.06 - 2023.03.06"
-    },
-    {
-        title: "Google Developers Group - KNU 4기 - Frontend",
-        period: "2024/09 ~ ongoing"
-    },
-    {
-      title: "React Team Study - Deep Dive",
-      period: "2024/09 ~ ongoing"
-    },
-    {
-      title: "우아한테크 코스",
-      period: "2024/10 ~ 2024/11"
-    }
-  ];
-  
-  export const experienceData: ActivityProps[] = [
-    {
-      title: "AI 산재 챗봇 Team Project- 2024 고용노동 공공데이터 활용 대회",
-      period: "2024/07 - 2024/08",
-      role: "Frontend"
-    },
-    {
-      title: "재활용 홈페이지 Team Project- 대구 환경청 협업 프로젝트",
-      period: "2024/07 - 2024/09",
-      role: "FullStack"
-    },
-    {
-      title: "개인 포트폴리오 및 기술 블로그 제작 with Next.JS",
-      period: "2024/08",
-      role: "Frontend"
-    },
-    {
-      title: "대구 상권 부루마블 게임 서비스 Team Project- 2024 대구를 빛내는 해커톤 장려상",
-      period: "2024/09 - 2024/09",
-      role: "Frontend"
-    },
-    {
-      title: "건축사 웹페이지 외주 제작",
-      period: "2024/09 - ongoing",
-      role: "FullStack"
-    },
-    {
-      title: "경매 웹페이지 제작 Team 프로젝트",
-      period: "2024/10 - ongoing",
-      role: "Frontend"
-    },
-    {
-      title: "AI 식습관 관리 시스템 - Google Vision ChallengeThon",
-      period: "2024/11",
-      role: "Frontend",
-      
-    }
-  ];
+  name: string;
+  birthDate: string;
+  location: string;
+  email: string;
+  university: string;
+  major: string;
+  achievements: string[];
+}
+
+export interface ActivityProps {
+  title: string;
+  period: string;
+  role?: string;
+}
+
+export const profileData: ProfileProps = {
+  name: 'Seong, Eunwoo',
+  birthDate: '2001. 01. 01',
+  location: 'Republic of Korea, Seoul (Daegu)',
+  email: 'Eunwoo1341@gmail.com',
+  university: 'Kyungbook National University',
+  major: ' - Computer Science',
+  achievements: [
+    'TOEIC - 935',
+    'Computer Specialist in Spreadsheet & Database (컴퓨터활용능력 1급)',
+  ],
+};
+
+export const extracurricularData: ActivityProps[] = [
+  {
+    title: 'Korean Augmentation To the United States Army, KATUSA',
+    period: '2020.09.06 - 2023.03.06',
+  },
+  {
+    title: 'Google Developers Group - KNU 4기 - Frontend',
+    period: '2024/09 ~ ongoing',
+  },
+  {
+    title: 'React Team Study - Deep Dive',
+    period: '2024/09 ~ ongoing',
+  },
+  {
+    title: '우아한테크 코스',
+    period: '2024/10 ~ 2024/12',
+  },
+  {
+    title: 'AI 기반 웹 서비스 개발 - (주)멜라카 현장실습',
+    period: '2024/12 - 2025/2',
+  },
+];
+
+export const experienceData: ActivityProps[] = [
+  {
+    title: 'AI 산재 챗봇 Team Project- 2024 고용노동 공공데이터 활용 대회',
+    period: '2024/07 - 2024/08',
+    role: 'Frontend',
+  },
+  {
+    title: '재활용 홈페이지 Team Project- 대구 환경청 협업 프로젝트',
+    period: '2024/07 - 2024/09',
+    role: 'FullStack',
+  },
+  {
+    title: '개인 포트폴리오 및 기술 블로그 제작 with Next.JS',
+    period: '2024/08',
+    role: 'Frontend',
+  },
+  {
+    title:
+      '대구 상권 부루마블 게임 서비스 Team Project- 2024 대구를 빛내는 해커톤 장려상',
+    period: '2024/09 - 2024/09',
+    role: 'Frontend',
+  },
+  {
+    title: '건축사 웹페이지 외주 제작',
+    period: '2024/09 - ongoing',
+    role: 'FullStack',
+  },
+  {
+    title: '경매 웹페이지 제작 Team 프로젝트',
+    period: '2024/10 - ongoing',
+    role: 'Frontend',
+  },
+  {
+    title: 'AI 식습관 관리 시스템 - Google Vision ChallengeThon',
+    period: '2024/11',
+    role: 'Frontend',
+  },
+  {
+    title: '토론 타임 키핑을 위한 타이머 서비스',
+    period: '2024/12 - ongoing',
+    role: 'Frontend',
+  },
+];
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -7,7 +7,13 @@ import { FaUniversity } from 'react-icons/fa';
 import { GiAchievement } from 'react-icons/gi';
 import SpaceBackground from '@/components/backgrounds/SpaceBackground';
 
-import { profileData, extracurricularData, experienceData, ProfileProps, ActivityProps } from './data';
+import {
+  profileData,
+  extracurricularData,
+  experienceData,
+  ProfileProps,
+  ActivityProps,
+} from './data';
 
 interface SectionTitleProps {
   icon: ReactIconType;
@@ -17,7 +23,9 @@ interface SectionTitleProps {
 const SectionTitle: React.FC<SectionTitleProps> = ({ icon: Icon, title }) => (
   <div className='flex items-center gap-[10px]'>
     <Icon size={25} className='md:size-30 text-blue-500' />
-    <h1 className='text-indigo-900 text-[24px] md:text-[30px] font-bold'>{title}</h1>
+    <h1 className='text-[24px] font-bold text-indigo-900 md:text-[30px]'>
+      {title}
+    </h1>
   </div>
 );
 
@@ -26,7 +34,7 @@ interface ProfileSectionProps {
 }
 
 const ProfileSection = ({ data }: ProfileSectionProps) => (
-  <div className='flex flex-col justify-center gap-y-[30px] md:gap-y-[40px] bg-white p-6 md:p-8 rounded-lg shadow-lg'>
+  <div className='flex flex-col justify-center gap-y-[30px] rounded-lg bg-white p-6 shadow-lg md:gap-y-[40px] md:p-8'>
     <SectionTitle icon={IoPersonOutline} title='Profile' />
     <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6'>
       <div className='flex items-center gap-2 md:gap-3'>
@@ -45,16 +53,18 @@ const ProfileSection = ({ data }: ProfileSectionProps) => (
         <FaUniversity size={22} className='md:size-25 text-indigo-600' />
         <div className='flex flex-col'>
           <h1 className='text-[18px] md:text-[20px]'>{data.university}</h1>
-          <h2 className='text-[16px] md:text-[18px] text-gray-600'>{data.major}</h2>
+          <h2 className='text-[16px] text-gray-600 md:text-[18px]'>
+            {data.major}
+          </h2>
         </div>
       </div>
     </div>
     <div className='mt-4 md:mt-6'>
-      <div className='flex items-center gap-2 md:gap-3 text-[20px] md:text-[22px] font-semibold mb-4 text-indigo-800'>
+      <div className='mb-4 flex items-center gap-2 text-[20px] font-semibold text-indigo-800 md:gap-3 md:text-[22px]'>
         <GiAchievement size={25} className='md:size-25 text-indigo-600' />
         <h1>Achievements</h1>
       </div>
-      <ul className='list-disc list-inside space-y-1 md:space-y-2'>
+      <ul className='list-inside list-disc space-y-1 md:space-y-2'>
         {data.achievements.map((achievement, index) => (
           <li key={index} className='text-[16px] md:text-[18px]'>
             {achievement}
@@ -62,9 +72,11 @@ const ProfileSection = ({ data }: ProfileSectionProps) => (
         ))}
       </ul>
     </div>
-    <div className='bg-gray-700/20 flex flex-col items-center gap-4 p-4 rounded-lg mt-8'>
-      <h1 className='text-indigo-900 text-[24px] md:text-[30px] font-bold'>⚡ Main Stack ⚡</h1>
-      <div className='flex flex-wrap gap-2 justify-center items-center'>
+    <div className='mt-8 flex flex-col items-center gap-4 rounded-lg bg-gray-700/20 p-4'>
+      <h1 className='text-[24px] font-bold text-indigo-900 md:text-[30px]'>
+        ⚡ Main Stack ⚡
+      </h1>
+      <div className='flex flex-wrap items-center justify-center gap-2'>
         <img
           src='https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'
           alt='React'
@@ -100,6 +112,10 @@ const ProfileSection = ({ data }: ProfileSectionProps) => (
           alt='Zustand'
           className='lg:flex-shrink-0'
         />
+        <img
+          src='https://img.shields.io/badge/-Storybook-FF4785?style=for-the-badge&logo=storybook&logoColor=white'
+          alt='Storybook'
+        />
       </div>
     </div>
   </div>
@@ -111,42 +127,48 @@ interface ActivitySectionProps {
 }
 
 const ActivitySection: React.FC<ActivitySectionProps> = ({ data, title }) => (
-  <div className='flex flex-col justify-center gap-y-[30px] md:gap-y-[40px] bg-white p-6 md:p-8 rounded-lg shadow-lg'>
+  <div className='flex flex-col justify-center gap-y-[30px] rounded-lg bg-white p-6 shadow-lg md:gap-y-[40px] md:p-8'>
     <SectionTitle icon={MdOutlineWork} title={title} />
     <div className='w-full'>
-      <table className='w-full table-fixed divide-y divide-gray-200' aria-label={title}>
+      <table
+        className='w-full table-fixed divide-y divide-gray-200'
+        aria-label={title}
+      >
         <thead className='bg-gray-50'>
           <tr>
             <th
               scope='col'
-              className='w-1/3 px-2 md:px-4 py-2 md:py-3 text-left text-xs md:text-sm text-gray-500 uppercase tracking-wider'
+              className='w-1/3 px-2 py-2 text-left text-xs uppercase tracking-wider text-gray-500 md:px-4 md:py-3 md:text-sm'
             >
               Activity
             </th>
             <th
               scope='col'
-              className='w-1/3 px-2 md:px-4 py-2 md:py-3 text-left text-xs md:text-sm text-gray-500 uppercase tracking-wider'
+              className='w-1/3 px-2 py-2 text-left text-xs uppercase tracking-wider text-gray-500 md:px-4 md:py-3 md:text-sm'
             >
               Role
             </th>
             <th
               scope='col'
-              className='w-1/3 px-2 md:px-4 py-2 md:py-3 text-left text-xs md:text-sm text-gray-500 uppercase tracking-wider'
+              className='w-1/3 px-2 py-2 text-left text-xs uppercase tracking-wider text-gray-500 md:px-4 md:py-3 md:text-sm'
             >
               Period
             </th>
           </tr>
         </thead>
-        <tbody className='bg-white divide-y divide-gray-200'>
+        <tbody className='divide-y divide-gray-200 bg-white'>
           {data.map((activity, index) => (
-            <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
-              <td className='px-2 md:px-4 py-2 md:py-4 text-xs md:text-sm font-medium text-gray-900 break-words'>
+            <tr
+              key={index}
+              className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}
+            >
+              <td className='break-words px-2 py-2 text-xs font-medium text-gray-900 md:px-4 md:py-4 md:text-sm'>
                 {activity.title}
               </td>
-              <td className='px-2 md:px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500 break-words'>
+              <td className='break-words px-2 py-2 text-xs text-gray-500 md:px-4 md:py-4 md:text-sm'>
                 {activity.role || '-'}
               </td>
-              <td className='px-2 md:px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500 break-words'>
+              <td className='break-words px-2 py-2 text-xs text-gray-500 md:px-4 md:py-4 md:text-sm'>
                 {activity.period}
               </td>
             </tr>
@@ -159,38 +181,45 @@ const ActivitySection: React.FC<ActivitySectionProps> = ({ data, title }) => (
 
 const AboutPage: React.FC = () => {
   return (
-    <main className='w-full min-h-screen text-black'>
+    <main className='min-h-screen w-full text-black'>
       <SpaceBackground />
-      <div className='w-full flex flex-col justify-center items-center gap-[30px] my-[60px]'>
-        <h1 className='z-10 p-[5px] md:p-[10px] dark:text-white text-[50px] md:text-[65px] font-bold items-center shadow-md shadow-purple-500 rounded-md'>
+      <div className='my-[60px] flex w-full flex-col items-center justify-center gap-[30px]'>
+        <h1 className='z-10 items-center rounded-md p-[5px] text-[50px] font-bold shadow-md shadow-purple-500 dark:text-white md:p-[10px] md:text-[65px]'>
           Portfolio
         </h1>
-        <h2 className='text-transparent lg:text-[30px] text-[20px] md:text-[25px] font-semibold bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text'>
+        <h2 className='bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-[20px] font-semibold text-transparent md:text-[25px] lg:text-[30px]'>
           항상 도전하는 정신을 가진 개발자
         </h2>
       </div>
-      <div className='w-full flex flex-col justify-center items-center gap-6 md:gap-8 px-4 lg:px-0 mb-16 md:mb-20 z-20'>
-        <div className='w-full max-w-screen-lg bg-white py-6 rounded-lg'>
+      <div className='z-20 mb-16 flex w-full flex-col items-center justify-center gap-6 px-4 md:mb-20 md:gap-8 lg:px-0'>
+        <div className='w-full max-w-screen-lg rounded-lg bg-white py-6'>
           <Image
             width={150}
             height={150}
             src='/profile.webp'
             alt='profile picture'
-            className='rounded-full object-cover w-[150px] h-[150px] md:w-[200px] md:h-[200px] mx-auto mb-4'
+            className='mx-auto mb-4 h-[150px] w-[150px] rounded-full object-cover md:h-[200px] md:w-[200px]'
             priority
           />
           <div className='text-center'>
-            <h1 className='text-indigo-900 text-[22px] md:text-[24px] font-bold'>EunWoo Seong</h1>
-            <h2 className='text-[16px] md:text-[18px] text-indigo-600'>Frontend Developer</h2>
-            <div className='flex justify-center gap-[8px] md:gap-[10px] text-indigo-500 items-center text-[14px] md:text-[15px] mt-2'>
+            <h1 className='text-[22px] font-bold text-indigo-900 md:text-[24px]'>
+              EunWoo Seong
+            </h1>
+            <h2 className='text-[16px] text-indigo-600 md:text-[18px]'>
+              Frontend Developer
+            </h2>
+            <div className='mt-2 flex items-center justify-center gap-[8px] text-[14px] text-indigo-500 md:gap-[10px] md:text-[15px]'>
               <CiLocationOn size={22} /> Seoul (Daegu), South Korea
             </div>
           </div>
         </div>
 
-        <div className='w-full max-w-screen-lg px-4 md:px-10 space-y-6 md:space-y-8'>
+        <div className='w-full max-w-screen-lg space-y-6 px-4 md:space-y-8 md:px-10'>
           <ProfileSection data={profileData} />
-          <ActivitySection data={extracurricularData} title='Extracurricular Activities' />
+          <ActivitySection
+            data={extracurricularData}
+            title='Extracurricular Activities'
+          />
           <ActivitySection data={experienceData} title='Project Experience' />
         </div>
       </div>
