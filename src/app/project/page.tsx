79105e7dcce4cefdabc3262d67aef6e9de41a70f
eunwoo-levi <eunwoo1341@gmail.com>
2025-01-07 02:43:15 +0900

    refactor: improve design and ui for project page

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
 import { getAllProjects } from '@/lib/mdx/getProject';
+import Image from 'next/image';
 
 export default async function ProjectPage() {
   const projects = await getAllProjects(); // 프로젝트 목록 가져오기
 
   return (
-    <main className='container mx-auto px-4 py-8'>
-      <h1 className='mb-6 text-3xl font-bold'>Projects</h1>
-      <ul className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
-        {projects.map((project) => (
-          <li
-            key={project.slug}
-            className='rounded-lg border p-4 shadow-sm transition-shadow hover:shadow-md'
+    <main className='flex w-full flex-col items-center px-4 py-8'>
+      {projects.map((project) => (
+        <div
+          key={project.slug}
+          className='mb-40 w-4/5 rounded-lg border p-4 shadow-sm transition-all duration-200 hover:scale-110'
+        >
+          <Link
+            href={`/project/${project.slug}`}
+            className='flex w-full flex-col items-center gap-4'
           >
-            <Link href={`/project/${project.slug}`} className='block'>
-              <div className='aspect-w-16 aspect-h-9 mb-4'>
-                <img
-                  src={project.frontmatter.thumbnail}
-                  alt={project.frontmatter.title}
-                  className='h-full w-full rounded object-cover'
-                />
-              </div>
-              <h2 className='mb-2 text-xl font-semibold'>
-                {project.frontmatter.title}
-              </h2>
-              <p className='text-gray-600'>{project.frontmatter.description}</p>
-              <p className='mt-2 text-sm text-gray-500'>
-                Published:{' '}
-                {new Date(project.frontmatter.publishDate).toLocaleDateString()}
-              </p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+            <Image
+              src={project.frontmatter.thumbnail}
+              alt={project.frontmatter.title}
+              width={400}
+              height={400}
+              className='mb-8 h-full w-full rounded object-cover'
+            />
+            <h2 className='mb-2 text-4xl font-bold'>
+              {project.frontmatter.title}
+            </h2>
+            <p className='text-2xl font-bold text-gray-600'>
+              {project.frontmatter.description}
+            </p>
+            <p className='mt-2 text-gray-500'>
+              Published:{' '}
+              {new Date(project.frontmatter.publishDate).toLocaleDateString()}
+            </p>
+          </Link>
+        </div>
+      ))}
     </main>
   );
 }
