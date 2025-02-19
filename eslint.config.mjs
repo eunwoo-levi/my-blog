3d7974b013d566c98a8c2bd51276b22fff7b7b37
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-19 23:33:02 +0900

    chore: pretty & eslint 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: ['prettier'],
+    extends: ['plugin:prettier/recommended'],
+    rules: {
+      'prettier/prettier': 'error', // Prettier 규칙 적용
+    },
+  },
 ];
 
 export default eslintConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // 루트 app 디렉토리
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}', // 추가된 feature 폴더 스캔
+    './src/shared/**/*.{js,ts,jsx,tsx,mdx}', // shared 폴더도 포함
+    './src/widgets/**/*.{js,ts,jsx,tsx,mdx}', // widgets 포함
+    './public/**/*.html', // 정적 HTML 파일에서 클래스 검색
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
