799b82198c7f22781dafe8e1693055bca167faa0
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-28 02:55:29 +0900

    feat: 카테고리 상수 배열 정의

---
+++ src/features/blog/model/data.ts
@@ -0,0 +1,9 @@
+export const CATEGORIES = [
+  { id: 1, name: 'Activities', path: 'activities' },
+  { id: 2, name: 'Algorithm', path: 'algorithm' },
+  { id: 3, name: 'JavaScript', path: 'javascript' },
+  { id: 4, name: 'Network', path: 'network' },
+  { id: 5, name: 'Next.js', path: 'next.js' },
+  { id: 6, name: 'React', path: 'react' },
+  { id: 7, name: 'Woowacourse', path: 'woowacourse' },
+] as const;
