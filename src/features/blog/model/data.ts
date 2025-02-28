1e8b83f858e352ad19607eaf5a0bbfa71f399b01
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-28 17:51:20 +0900

    feat: mdx 관련 로직 Type 정의

--- src/features/blog/model/data.ts
+++ src/features/blog/model/data.ts
@@ -3,7 +3,7 @@ export const CATEGORIES = [
   { id: 2, name: 'Algorithm', path: 'algorithm' },
   { id: 3, name: 'JavaScript', path: 'javascript' },
   { id: 4, name: 'Network', path: 'network' },
-  { id: 5, name: 'Next.js', path: 'next.js' },
+  { id: 5, name: 'Next.js', path: 'nextjs' },
   { id: 6, name: 'React', path: 'react' },
   { id: 7, name: 'Woowacourse', path: 'woowacourse' },
 ] as const;
---
+++ src/shared/lib/mdx/type.ts
@@ -0,0 +1,28 @@
+export interface BlogFrontMatter {
+  title: string;
+  author: string;
+  thumbnail: string;
+  publishDate: string;
+  categoryId: number;
+}
+
+export interface PostData {
+  frontmatter: BlogFrontMatter;
+  slug: string;
+  category: string;
+}
+
+export interface ProjectFrontMatter {
+  id: number;
+  title: string;
+  description: string;
+  role: string;
+  thumbnail: string;
+  projectDate: string;
+  url: string;
+}
+
+export interface ProjectData {
+  frontmatter: ProjectFrontMatter;
+  slug: string;
+}
