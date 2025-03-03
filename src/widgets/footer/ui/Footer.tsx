4e5c134ba8d96465a129af2768ed063ef5ef33b9
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-03 22:03:41 +0900

    style: Footer 글자 크기 확대

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -2,11 +2,11 @@ import { MdOutlineEmail } from 'react-icons/md';
 
 export default function Footer() {
   return (
-    <footer className='flex h-[120px] w-full flex-col justify-center gap-4 bg-[#242729] pl-28'>
-      <span className='text-[#bbc0c4]'>Copyright ⓒ S. Eunwoo</span>
+    <footer className='flex h-[120px] w-full flex-col justify-center gap-4 bg-[#242729] pl-32'>
+      <span className='text-xl text-[#bbc0c4]'>COPYRIGHT ⓒ eunwoo-levi</span>
       <div className='flex gap-2'>
         <MdOutlineEmail size={25} />
-        <span className='text-[#848d95]'>eunwoo1341@gmail.com</span>
+        <span className='text-lg text-[#848d95]'>eunwoo1341@gmail.com</span>
       </div>
     </footer>
   );
