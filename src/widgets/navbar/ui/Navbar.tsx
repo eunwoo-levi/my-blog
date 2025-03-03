d0e4e584f2bcfecca4f8110c4a9520bbb96657a1
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-04 01:09:39 +0900

    feat: 닉네임 로고 navbar에 추가

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -6,6 +6,7 @@ import { navbarList } from '../model/data';
 import MobileNavbar from './MobileNavbar';
 import { FaGithub } from 'react-icons/fa';
 import DarkModeButton from './DarkModeButton';
+import Image from 'next/image';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,9 +14,16 @@ export default function Navbar() {
   return (
     <nav className='sticky left-0 top-0 z-50 flex h-[70px] w-full bg-white/90 shadow-md dark:bg-black/90'>
       <div className='mx-auto flex w-full items-center justify-between px-6 md:px-10 lg:px-20'>
-        <div className='flex items-center'>
-          <Link href='/' className='duration-200 hover:scale-110'>
-            <h1 className='text-xl font-bold md:text-2xl'>Developer Eunwoo</h1>
+        <div className='flex h-full items-center'>
+          <Link href='/' className='flex h-full items-center gap-2 duration-200 hover:scale-110'>
+            <Image
+              src='/levi.webp'
+              width={40}
+              height={40}
+              className='h-10 w-10 rounded-full object-cover'
+              alt='levi'
+            />
+            <h1 className='text-xl font-bold md:text-2xl'>개발자 리바이</h1>
           </Link>
         </div>
 
