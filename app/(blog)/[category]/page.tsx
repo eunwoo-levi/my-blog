d02ec7230df267f1a14e807167261605bc9e3161
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-05 00:09:13 +0900

    feat: 카테고리 별 블로그 필터 로직 구현

--- app/(blog)/[category]/page.tsx
+++ app/(blog)/[category]/page.tsx
@@ -1,4 +1,18 @@
+import { BlogContainer } from '@/src/features/blog';
+import { CATEGORIES } from '@/src/features/blog/model/data';
+import { getAllPosts } from '@/src/shared/lib/mdx/getBlog';
+
 export const revalidate = 3600;
-export default function CategoryPage() {
-  return <div>카테고리 페이지</div>;
+
+export default async function CategoryPage({ params }: { params: { category: string } }) {
+  const { category } = await params;
+  const categoryId = CATEGORIES.find((c) => c.path === category)?.id;
+
+  const posts = await getAllPosts(categoryId);
+
+  return (
+    <main className='w-full'>
+      <BlogContainer posts={posts} />
+    </main>
+  );
 }
