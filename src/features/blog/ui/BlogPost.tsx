c8624f390ae97517d56b30ae085bf7175ee2efb9
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-05 14:50:24 +0900

    chore: thumbnail 사진 추가 및 적용

--- src/features/blog/ui/BlogPost.tsx
+++ src/features/blog/ui/BlogPost.tsx
@@ -5,19 +5,19 @@ import Link from 'next/link';
 export default function BlogPost({ post }: { post: PostData }) {
   return (
     <Link href={`/${post.category}/${post.slug}`} className='block'>
-      <div className='h-[380px] w-[350px] transform overflow-hidden rounded-lg bg-white/20 shadow-lg transition duration-200 hover:scale-105 hover:text-purple-500'>
+      <div className='h-[400px] w-[350px] transform overflow-hidden rounded-lg bg-white/20 shadow-lg transition duration-200 hover:scale-105 hover:text-purple-500'>
         <Image
           src={post.frontmatter.thumbnail}
           width={280}
           height={160}
           alt={`${post.frontmatter.title} thumbnail`}
-          className='h-[160px] w-full object-contain'
+          className='h-[200px] w-full object-cover'
         />
 
-        <div className='flex h-[calc(380px-160px)] flex-col justify-between p-4'>
+        <div className='flex h-[calc(400px-200px)] flex-col justify-between p-4'>
           <div>
-            <span className='text-lg text-gray-400'>#{post.category}</span>
-            <h2 className='mt-2 text-xl font-bold'>{post.frontmatter.title}</h2>
+            <span className='text-xl text-gray-400'>#{post.category}</span>
+            <h2 className='mt-2 text-2xl font-bold'>{post.frontmatter.title}</h2>
           </div>
 
           <p className='text-gray-400'>{post.frontmatter.publishDate}</p>
