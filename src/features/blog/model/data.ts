c6cff1fdf7bad28dea7c9bce2c9abb0f926a659b
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-05 22:28:59 +0900

    fix: typescript 카테고리 경로 수정

--- src/features/blog/model/data.ts
+++ src/features/blog/model/data.ts
@@ -2,7 +2,7 @@ export const CATEGORIES = [
   { id: 0, name: 'All', path: '' },
   { id: 1, name: 'Activities', path: 'activities' },
   { id: 2, name: 'Algorithm', path: 'algorithm' },
-  { id: 3, name: 'JavaScript', path: 'javascript' },
+  { id: 3, name: 'TypeScript', path: 'typescript' },
   { id: 4, name: 'Network', path: 'network' },
   { id: 5, name: 'Next.js', path: 'nextjs' },
   { id: 6, name: 'React', path: 'react' },
