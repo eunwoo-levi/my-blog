2499912a045c43653360ad378a0be71f214f0051
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-21 01:37:48 +0900

    blog: 이벤트 루프 블로그 업데이트

--- src/shared/lib/mdx/getPortfolio.ts
+++ src/shared/lib/mdx/getPortfolio.ts
@@ -2,18 +2,16 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
-const portfolioDirectory = path.join(process.cwd(), 'portfolio'); // 'portfolio' 디렉토리 경로
+const portfolioDirectory = path.join(process.cwd(), 'portfolio');
 
 export async function getPortfolio() {
-  // 포트폴리오 디렉토리에서 MDX 파일 찾기
   const files = fs.readdirSync(portfolioDirectory).filter((file) => file.endsWith('.mdx'));
 
   if (files.length === 0) {
-    return null; // 파일이 없으면 null 반환
+    return null;
   }
 
-  // 첫 번째 MDX 파일 읽기
-  const firstFile = files[0]; // 첫 번째 파일 선택
+  const firstFile = files[0];
   const filePath = path.join(portfolioDirectory, firstFile);
   const fileContent = fs.readFileSync(filePath, 'utf-8');
 
