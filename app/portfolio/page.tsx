0b5ee6dfb7e2f75b013f85b327b19828041ad1f3
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-20 13:04:01 +0900

    feat: portfolio content 업데이트

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -6,16 +6,16 @@ export default async function PortfolioPage() {
   const post = await getPortfolio();
 
   if (!post) {
-    notFound(); // 파일이 없으면 404 페이지로 이동
+    notFound();
   }
 
   return (
-    <main className='mx-auto max-w-4xl px-6 py-12'>
-      <article className='prose prose-lg dark:prose-invert'>
+    <div className='flex w-full flex-col items-center py-12'>
+      <article className='prose prose-lg prose-invert w-full max-w-7xl rounded-xl bg-gray-900 px-6'>
         <h1>{post.frontmatter.title}</h1>
         <p className='text-gray-500'>{post.frontmatter.publishDate}</p>
         <MDXRemote source={post.content} />
       </article>
-    </main>
+    </div>
   );
 }
