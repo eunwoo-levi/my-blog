56569cd24cfd1d3d439617b498b040f66348b130
eunwoo-levi <eunwoo1341@gmail.com>
2025-06-13 11:04:15 +0900

    fix: fix type error

--- app/(blog)/search/page.tsx
+++ app/(blog)/search/page.tsx
@@ -3,10 +3,18 @@ import BlogSearchBar from '@/src/features/blog/ui/BlogSearchBar';
 import { getSearchedPostsByKeyword } from '@/src/shared/lib/mdx/getBlog';
 import { notFound } from 'next/navigation';
 
+type PageProps = {
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
 export const revalidate = 3600;
 
-export default async function Page({ searchParams }: { searchParams?: { keyword?: string } }) {
-  const keyword = searchParams?.keyword?.trim();
+export default async function Page({ searchParams }: PageProps) {
+  // searchParams를 await로 받아야 합니다
+  const resolvedSearchParams = await searchParams;
+  const keyword =
+    typeof resolvedSearchParams?.keyword === 'string' ? resolvedSearchParams.keyword.trim() : '';
+
   if (!keyword) return notFound();
 
   const searchedPosts = await getSearchedPostsByKeyword(keyword);
